edc5325919be6ce960eb50ab5ab7f4baf7c5577d
kailash <kailash414243@gmail.com>
2022-11-17 12:07:47 +0530

    log in page validation using username and password dom done

--- script.js
+++ script.js
@@ -18,6 +18,10 @@ function decremNumFunc() {
 
 */
 
+
+
+
+
 // ====PRACTICE CODE : #2 ========
 /*
 let myTitle = document.getElementById('title');
@@ -32,4 +36,113 @@ function hideContent() {
 }
 */
 
+
+
+
+
+
+
+
+
 // ====PRACTICE CODE : #3 ========
+/*
+let downBtn = document.getElementById('downloadBtn');
+
+
+function downloadResume() {
+    downBtn = window.open('asset/css.pdf');
+}
+*/
+
+
+
+
+
+
+
+
+
+
+
+// ====PRACTICE CODE : #4 ========
+/*
+let disNone = document.querySelector('.main');
+
+disNone.style.display = 'none';
+
+document.getElementById('load').classList.add('loader');
+
+setTimeout(() => {
+    document.getElementById('load').classList.remove('loader');
+    disNone.style.display = 'block';
+}, 4000);
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// ====PRACTICE CODE : #5 ========
+/*
+let errorSms = document.getElementById('error-messege');
+errorSms.style.display = "none";
+
+
+function logInSucess() {
+
+    var inputUserName = document.getElementById('user-name').value;
+
+    var inputUserPassword = document.getElementById('user-password').value;
+
+    if (inputUserName === "kailash" && inputUserPassword === "kailash") {
+        alert("sucess");
+        let myName = prompt("Enter your name: ");
+        if (myName === "kailash") {
+            window.open("https://www.kailashkumar.in", '_target');
+        }
+    } else if (inputUserName === "aryan" && inputUserPassword === "aryan") {
+        alert("sucess");
+        let myName = prompt("Enter your name: ");
+        if (myName === "kailash") {
+            window.open("https://www.kailashkumar.in", '_target');
+        }
+    } else if (inputUserName === "laxmi" && inputUserPassword === "laxmi") {
+        alert("sucess");
+        let myName = prompt("Enter your name: ");
+        if (myName === "kailash") {
+            window.open("https://www.kailashkumar.in", '_target');
+        }
+    } else {
+        errorSms.style.display = "block";
+    }
+}
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// ====PRACTICE CODE : #6 ========
